851ff0bf9278d356539c2ec12427a8c76a2bf9b5
Paul <guilbert.paul@gmail.com>
2021-01-17 12:33:06 +0100

    Add test on start fail.

--- src/__tests__/useSignalR.test.tsx
+++ src/__tests__/useSignalR.test.tsx
@@ -1,14 +1,32 @@
 import { useSignalR } from "../useSignalR";
 import { renderHook, act } from '@testing-library/react-hooks'
-import { HubConnectionState } from "@microsoft/signalr";
+import { HubConnection, HubConnectionState } from "@microsoft/signalr";
 
 describe('useSignalR', () => {
+    const hubConnectionMock = { 
+        state: HubConnectionState.Disconnected,
+        start: jest.fn(() => new Promise(() => {})),
+        stop: jest.fn(() => new Promise(() => {})),
+        onclose: jest.fn((callback: (error?: Error) => void) => {}),
+        onreconnected: jest.fn((callback: (error?: Error) => void) => {}),
+        onreconnecting: jest.fn((callback: (error?: Error) => void) => {}),
+    } as unknown as HubConnection;
 
-    it('should render when the connection is undefined', () => {
+    it('should render when the connection is undefined.', () => {
         const { result } = renderHook(() => useSignalR());
 
         expect(result.current.hubConnectionState).toBe(HubConnectionState.Disconnected);
         expect(result.current.error).toBeUndefined();
     });
 
+    it("should provide the error when start() fail.", async () => {
+        const error = new Error("Test");
+        const mock = { ... hubConnectionMock, start: jest.fn(() => new Promise(() => { throw error; })) } as unknown as HubConnection;
+        const { start: startFn } = mock;
+        
+        const { result, waitForNextUpdate } = renderHook(() => useSignalR(mock));
+        await waitForNextUpdate();
+        expect(startFn).toBeCalled();
+        expect(result.current.error).toBe(error);
+    });
 });
