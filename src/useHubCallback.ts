77c1727fcf246f21d625313c7ebe752880a43dcf
Paul <guilbert.paul@gmail.com>
2021-01-25 21:53:58 +0100

    prevent updating state on unmounted component.

--- src/useHubCallback.ts
+++ src/useHubCallback.ts
@@ -1,5 +1,5 @@
 import { HubConnection } from "@microsoft/signalr";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 type HubCallbackState<T = any> = {
     loading: boolean,
@@ -10,21 +10,35 @@ const initialState : HubCallbackState = {
     loading: false
 }
 
+/**
+ * Invokes a hub method on the server and provide the async state (loading & error)
+ * @param hubConnection The hub connection to use
+ * @param methodName The name of the server method to invoke.
+ */
 export function useHubCallback<T>(hubConnection: HubConnection, methodName: string) {
     const [state, setState] = useState<HubCallbackState<T>>(initialState);
+    const isMounted = useRef(true);
+
+    const setStateIfMounted: typeof setState = useCallback((value) => {
+        if(isMounted.current) {
+            setState(value);
+        }
+    }, []); 
 
     const invoke = useCallback(async (...args: any[]) => {
-        setState(s => ({...s, loading: true}));
+        setStateIfMounted(s => ({...s, loading: true}));
 
         try {
             const data = await hubConnection.invoke<T>(methodName, ...args);
-            setState(s => ({...s, data: data, loading: false, error: undefined}));
+            setStateIfMounted(s => ({...s, data: data, loading: false, error: undefined}));
             return data;
         }
         catch(e) {
-            setState(s => ({...s, error: e, loading: false}));
+            setStateIfMounted(s => ({...s, error: e, loading: false}));
         }
     }, [hubConnection, methodName]);
+
+    useEffect(() => () => {isMounted.current = false}, []);
 
     return { invoke, ...state };
 }
